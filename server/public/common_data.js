b2fdca86c2ec7b842c008e55d37ead9b8201703c
Markus Klink <justjoheinz@gmail.com>
2014-04-13 14:13:40 +0200

    work with common_data

--- server/public/common_data.js
+++ server/public/common_data.js
@@ -2,18 +2,32 @@
  * Created by markusklink on 13.04.14.
  */
 
-var initialConfig = function () {
+var createGameConfig = function () {
     return {
         gameId: 0,
         playerId: 0,
         gameTime: Date.now(),
-        latenz: undefined,
         players: [],
         ball: {
             x: 0,
             y: 0,
             dx: 0,
             dy: 0,
-            speed: 0
+            gameTime: undefined
         }};
+}
+
+var createClientConfig = function() {
+    return {
+        latency: undefined
+    }
+}
+var createPlayer = function() {
+    return {
+        playerId : undefined,
+        paddle : {
+            pos: undefined,
+            gameTime: undefined
+        }
+    }
 }
--- server/public/pongclient.js
+++ server/public/pongclient.js
@@ -2,92 +2,93 @@
  * Created by markusklink on 13.04.14.
  */
 
+/**
+ * Location of the game server
+ * @type {*|io.Socket}
+ */
 var gameServer = io.connect('http://localhost:3000');
 
-var player = {
-    id: 0,
-    paddlePos: 0
-}
+/**
+ * Configuration object.
+ */
+var gameCfg = createGameConfig()
+var clientCfg = createClientConfig()
 
-var ball = {
-    x : 0,
-    y : 0,
-    dx : 0,
-    dy : 0,
-    speed : 0
-}
 
-var config = {
-    gameId : 0,
-    playerId: 0,
-    myTime : Date.now(),
-    gameTime : Date.now(),
-    latenz : undefined
-}
-
-var setConfig = function(config, data) {
+var setConfig = function (config, data) {
     config.gameId = data.gameId
     config.playerId = data.playerId
+    config.gameTime = data.gameTime
 }
 
-gameServer.on("ackGame", function(data){
-    console.log(data)
-    setConfig(config, data)
-    console.log(config)
-})
-
-gameServer.on("ackJoin", function(data){
-    console.log(data)
-    setConfig(config, data)
-    console.log(config)
-
-})
-
-gameServer.on("ballUpdate", function(data){
-    console.log(data)
-    console.log("TODO")
-    ball = data.ball
-    console.log("TODO gametime?")
-    console.log(ball)
-})
-
-var createGame = function() {
-    var time = Date.now()
-    gameServer.emit("createGame", {"time" : time })
-    gameServer.on("ackCreateGame", function(data) {
-        console.log(data);
-    })
-}
-
-var ping = function(config) {
-    config.myTime = Date.now()
-    gameServer.emit("ping" , function() {
-        console.log("pong")
-        var currentLatenz = Date.now() - config.myTime
-        if (config.latenz) {
-           config.latenz = ( config.latenz + currentLatenz ) / 2
-        } else {
-            config.latenz = currentLatenz
-        }
+var registerCallbacks = function () {
+    gameServer.on("ackCreateGame", function (data) {
+        console.log(data)
+        setConfig(config, data)
         console.log(config)
     })
+
+    gameServer.on("ackJoinGame", function (data) {
+        console.log(data)
+        setConfig(config, data)
+        console.log(config)
+    })
+
+    gameServer.on("ballUpdate", function (data) {
+        console.log(data)
+        console.log("TODO")
+        ball = data.ball
+        console.log("TODO gametime?")
+        console.log(ball)
+    })
 }
 
-var ballUpdate = function(ball, config) {
+var createGame = function () {
+    config.gameTime = Date.now()
+    gameServer.emit("createGame", {"gameTime": config.gameTime })
+}
+
+var joinGame = function () {
+    gameServer.emit("joinGame", {"gameTime": config.gameTime })
+}
+
+/**
+ *
+ * @param clientCfg
+ */
+var ping = function (clientCfg) {
+    config.myTime = Date.now()
+    gameServer.emit("ping", Date.now(), function (clientTime) {
+        console.log("pong")
+        var currentLatenz = Date.now() - clientTime
+        if (config.latency) {
+            config.latency = ( config.latency + currentLatenz ) / 2
+        } else {
+            config.latency = currentLatenz
+        }
+        console.log("Client Config " + config)
+    })
+}
+
+var ballUpdate = function (config) {
     gameServer.emit("ballUpdate", {
-        "ball" : ball,
-        "gameTime": config.gameTime,
-        "gameId" : config.gameId,
+        "ball": config.ball,
+        "gameId": config.gameId,
         "playerId": config.playerId
     })
 }
 
-var paddleUpdate = function(paddle, config) {
-    gameServer.emit("paddleUpdate", player)
+var paddleUpdate = function (config) {
+    gameServer.emit("paddleUpdate", config.players[config.playerId])
 }
 
+registerCallbacks()
 
 createGame()
+
+joinGame()
+
+ping(config)
 
 ping(config)
 
