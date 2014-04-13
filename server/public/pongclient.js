012757cec20ceea476a243bff0001b4d6a9e7e60
Markus Klink <justjoheinz@gmail.com>
2014-04-13 14:47:24 +0200

    fixed warnings

--- server/public/pongclient.js
+++ server/public/pongclient.js
@@ -11,28 +11,28 @@ var gameServer = io.connect('http://localhost:3000');
 /**
  * Configuration object.
  */
-var gameCfg = createGameConfig()
-var clientCfg = createClientConfig()
+var gameCfg = createGameConfig();
+var clientCfg = createClientConfig();
 
 
 var setConfig = function (config, data) {
-    config.gameId = data.gameId
-    config.playerId = data.playerId
-    config.gameTime = data.gameTime
-}
+    config.gameId = data.gameId;
+    config.playerId = data.playerId;
+    config.gameTime = data.gameTime;
+};
 
 var registerCallbacks = function () {
     gameServer.on("ackCreateGame", function (data) {
-        console.log(data)
-        setConfig(config, data)
-        console.log(config)
-    })
+        console.log(data);
+        setConfig(gameCfg, data);
+        console.log(gameCfg);
+    });
 
     gameServer.on("ackJoinGame", function (data) {
-        console.log(data)
-        gameCfg = data.game
+        console.log(data);
+        gameCfg = data.game;
         // TODO gametime
-    })
+    });
 
     gameServer.on("ballUpdate", function (data) {
         console.log(data);
@@ -40,82 +40,85 @@ var registerCallbacks = function () {
         ball = data.ball;
         console.log("TODO gametime?");
         console.log(ball);
-    })
+    });
 
-    gameServer.on("paddleUpdate", function(data) {
-      console.log(data);
-      gameCfg.players[data.player.playerId].paddle = data.player.paddle;
-    })
+    gameServer.on("paddleUpdate", function (data) {
+        console.log(data);
+        gameCfg.players[data.player.playerId].paddle = data.player.paddle;
+    });
 
     gameServer.on("playerJoined", function (data) {
         console.log(data);
         console.log("TODO");
         // TODO gametime
         gameCfg = data.game;
-    })
-}
+    });
+};
 
 var createGame = function (config) {
-    config.gameTime = Date.now()
-    config.players.push(createPlayer())
-    config.players[0].playerId = 0
+    config.gameTime = Date.now();
+    config.players.push(createPlayer());
+    config.players[0].playerId = 0;
     config.players[0].paddle = {
         pos: 0,
         gameTime: config.gameTime
-    }
-    gameServer.emit("createGame", {"gameTime": config.gameTime , paddle: config.players[0].paddle})
-}
+    };
+    gameServer.emit("createGame", {"gameTime": config.gameTime, paddle: config.players[0].paddle});
+};
 
 var joinGame = function (config) {
-    gameServer.emit("joinGame", {"gameId": config.gameId , paddle :{
-        pos: 0,
-        gameTime: undefined
-    }})
-}
+    gameServer.emit("joinGame", {
+        gameId: config.gameId,
+        paddle: {
+            pos: 0,
+            gameTime: undefined
+        }
+    });
+};
 
 /**
  *
  * @param clientCfg
  */
 var ping = function (clientCfg) {
-    config.myTime = Date.now()
+    clientCfg.myTime = Date.now();
     gameServer.emit("ping", Date.now(), function (clientTime) {
-        console.log("pong")
-        var currentLatenz = Date.now() - clientTime
-        if (config.latency) {
-            config.latency = ( config.latency + currentLatenz ) / 2
+        console.log("pong");
+        var currentLatenz = Date.now() - clientTime;
+        if (clientCfg.latency) {
+            clientCfg.latency = ( clientCfg.latency + currentLatenz ) / 2;
         } else {
-            config.latency = currentLatenz
+            clientCfg.latency = currentLatenz;
         }
-        console.log("Client Config " + config)
+        console.log("Client Config " + config);
     })
-}
+};
 
 var ballUpdate = function (config) {
     gameServer.emit("ballUpdate", {
-        "ball": config.ball,
+        ball: config.ball,
         "gameId": config.gameId,
         "playerId": config.playerId
-    })
-}
+    });
+};
 
 var paddleUpdate = function (config) {
     gameServer.emit("paddleUpdate", {
         gameId: config.gameId,
         player: config.players[config.playerId]
-    })
-}
+    });
+};
 
-registerCallbacks()
+registerCallbacks();
 
-createGame(gameCfg)
+createGame(gameCfg);
 
-joinGame()
+joinGame(gameCfg);
 
-ping(config)
+ping(clientCfg);
 
-ping(config)
+ping(clientCfg);
 
-ballUpdate(ball, config)
+ballUpdate(gameCfg);
 
-paddleUpdate(player, config)
+paddleUpdate(gameCfg);
