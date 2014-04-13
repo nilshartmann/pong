c03a958ff91dc44b8bd17f2cd1fd13ec57ac945d
Markus Klink <justjoheinz@gmail.com>
2014-04-13 12:35:11 +0200

    ball and paddle updates

--- server/public/pongclient.js
+++ server/public/pongclient.js
@@ -22,7 +22,7 @@ var config = {
     playerId: 0,
     myTime : Date.now(),
     gameTime : Date.now(),
-    latenz : 0
+    latenz : undefined
 }
 
 var setConfig = function(config, data) {
@@ -43,6 +43,14 @@ gameServer.on("ackJoin", function(data){
 
 })
 
+gameServer.on("ballUpdate", function(data){
+    console.log(data)
+    console.log("TODO")
+    ball = data.ball
+    console.log("TODO gametime?")
+    console.log(ball)
+})
+
 var createGame = function() {
     var time = Date.now()
     gameServer.emit("createGame", {"time" : time })
@@ -55,12 +63,34 @@ var ping = function(config) {
     config.myTime = Date.now()
     gameServer.emit("ping" , function() {
         console.log("pong")
-        var timeNow = Date.now()
-        config.latenz = timeNow - config.myTime
+        var currentLatenz = Date.now() - config.myTime
+        if (config.latenz) {
+           config.latenz = ( config.latenz + currentLatenz ) / 2
+        } else {
+            config.latenz = currentLatenz
+        }
         console.log(config)
     })
 }
 
+var ballUpdate = function(ball, config) {
+    gameServer.emit("ballUpdate", {
+        "ball" : ball,
+        "gameTime": config.gameTime,
+        "gameId" : config.gameId,
+        "playerId": config.playerId
+    })
+}
+
+var paddleUpdate = function(paddle, config) {
+    gameServer.emit("paddleUpdate", player)
+}
+
+
+createGame()
 
 ping(config)
 
+ballUpdate(ball, config)
+
+paddleUpdate(player, config)
